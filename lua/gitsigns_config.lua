8e4911288bfe0a4ed028ebb49abd71be92954231
sushi star <wweeii.du@gmail.com>
2024-08-13 21:51:54 -0700

    updated

--- lua/gitsigns_config.lua
+++ lua/gitsigns_config.lua
@@ -20,11 +20,11 @@ require('gitsigns').setup {
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
+    delay = 100,
     ignore_whitespace = false,
     virt_text_priority = 100,
   },
-  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  current_line_blame_formatter = '<abbrev_sha> <author>, <author_time:%Y-%m-%d> - <summary>',
   current_line_blame_formatter_opts = {
     relative_time = false,
   },
